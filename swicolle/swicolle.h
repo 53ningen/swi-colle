80cb386cc2dcfb5a0aa282d83579f5d39bb92165
53nigen <korun24276@gmail.com>
2015-05-28 23:12:15 +0900

    initial commit

---
+++ swicolle/swicolle.h
@@ -0,0 +1,18 @@
+//
+//  swicolle.h
+//  swicolle
+//
+//  Copyright (c) 2015年 gomi_ningen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for swicolle.
+FOUNDATION_EXPORT double swicolleVersionNumber;
+
+//! Project version string for swicolle.
+FOUNDATION_EXPORT const unsigned char swicolleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <swicolle/PublicHeader.h>
+
+
